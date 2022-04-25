ba8d0ab34cf96300934db50de36795904632187f
JoelHauser <joel.hauser@gmail.com>
2022-04-25 17:07:41 -0400

    fixing bugs

--- index.js
+++ index.js
@@ -320,3 +320,66 @@ function promptEmployeeRole(employeeChoices, roleChoices) {
       );
     });
 }
+
+function addRole() {
+  var query = `SELECT d.id, d.name, r.salary AS budget
+    FROM employee e
+    JOIN role r
+    ON e.role_id = r.id
+    JOIN department d
+    ON d.id = r.department_id
+    GROUP BY d.id, d.name`;
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    const departmentChoices = res.map(({ id, name }) => ({
+      value: id,
+      name: `${id} ${name}`,
+    }));
+    console.table(res);
+    console.log("Array for Department(s)");
+
+    promptAddRole(departmentChoices);
+  });
+}
+
+function promptAddRole(departmentChoices) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleTitle",
+        message: "What is the role title?",
+      },
+      {
+        type: "input",
+        name: "roleSalary",
+        message: "What is the salary?",
+      },
+      {
+        type: "list",
+        name: "departmentId",
+        message: "What department?",
+        choices: departmentChoices,
+      },
+    ])
+    .then(function (answer) {
+      var query = `INSERT INTO role SET ?`;
+      connection.query(
+        query,
+        {
+          title: answer.title,
+          salary: answer.salary,
+          department_id: answer.departmentId,
+        },
+        function (err, res) {
+          if (err) throw err;
+
+          console.table(res);
+          console.log("Role has been inserted");
+          firstConnect();
+        }
+      );
+    });
+}
